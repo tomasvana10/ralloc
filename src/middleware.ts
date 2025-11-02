ec05c0c1411ce8b49451f09cde069947b2e21f7b
Tomas Vana <tomasvana@yahoo.com>
2025-11-02 17:35:30 +1100

    add missing api paths to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,5 +3,5 @@ import type { MiddlewareConfig } from "next/server";
 export { auth as middleware } from "@/auth";
 
 export const config: MiddlewareConfig = {
-  matcher: ["/", "/api/ws/:path*", "/api/session/:path*"],
+  matcher: ["/", "/api/ws/:path*", "/api/sessions/:path*", "/api/host/:path*"],
 };
