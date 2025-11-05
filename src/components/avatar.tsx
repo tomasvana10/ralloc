c9e556653cab4834cf9d9d714e41c9ac57b8be98
Tomas Vana <tomasvana@yahoo.com>
2025-11-06 00:53:18 +1100

    improve avatar code

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,24 +1,33 @@
 "use client";
 
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import * as AvatarPrimitive from "@radix-ui/react-avatar";
 
 export function ClientAvatar({
   image,
   name,
+  imageProps,
 }: {
   image?: string;
   name?: string | null;
+  imageProps?: React.ComponentProps<typeof AvatarPrimitive.Image>;
 }) {
+  const initials =
+    name
+      ?.split(" ")
+      .map(p => p[0])
+      .join("")
+      .toUpperCase() ?? "?";
+
   return (
     <Avatar>
-      <AvatarImage src={image} referrerPolicy="no-referrer" alt={`${name}'s avatar`} />
-      <AvatarFallback>
-        {name
-          ?.split(" ")
-          .map((p) => p[0])
-          .join("")
-          .toUpperCase() ?? "?"}
-      </AvatarFallback>
+      <AvatarImage
+        src={image}
+        referrerPolicy="no-referrer"
+        alt={`${name}'s avatar`}
+        {...imageProps}
+      />
+      <AvatarFallback>{initials}</AvatarFallback>
     </Avatar>
   );
 }
