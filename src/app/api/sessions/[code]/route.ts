06d5e3618e1375e9fcf38ec4c3885abb1a8518bb
Tomas Vana <tomasvana@yahoo.com>
2025-11-12 16:22:03 +1100

    use .strict() for patch data validation
    using just .partial() let the client submit unknown properties in their data! very bad

--- src/app/api/sessions/[code]/route.ts
+++ src/app/api/sessions/[code]/route.ts
@@ -50,7 +50,7 @@ export async function PATCH(req: Request, { params }: { params: Params }) {
     );
 
   const body = await req.json();
-  const parseResult = sessionCreateSchema.partial().safeParse(body);
+  const parseResult = sessionCreateSchema.partial().strict().safeParse(body);
 
   if (!parseResult.success) return getZodSafeParseErrorResponse(parseResult);
 
