54115f33c0df2d01152f0c48311f2f4a887ae64a
Tomas Vana <tomasvana@yahoo.com>
2025-11-15 21:16:21 +1100

    minor c2s and s2c messaging type updates

--- src/lib/session/c2s-messaging.ts
+++ src/lib/session/c2s-messaging.ts
@@ -4,16 +4,22 @@ import { GroupSeed } from "../seed";
 export const GroupSessionC2SCode = z.enum(["JoinGroup", "LeaveGroup"]);
 export type GroupSessionC2SCode = z.infer<typeof GroupSessionC2SCode>;
 
+/**
+ * Payload to join a group
+ */
 export const GroupSessionC2SJoinGroupPayload = z.object({
-  type: z.literal(GroupSessionC2SCode.enum.JoinGroup),
+  code: z.literal(GroupSessionC2SCode.enum.JoinGroup),
   groupName: z.string().min(1).max(GroupSeed.MAX_PART_LENGTH),
 });
 
+/**
+ * Payload to leave a group
+ */
 export const GroupSessionC2SLeaveGroupPayload = z.object({
-  type: z.literal(GroupSessionC2SCode.enum.LeaveGroup),
+  code: z.literal(GroupSessionC2SCode.enum.LeaveGroup),
 });
 
-export const GroupSessionC2SPayload = z.discriminatedUnion("type", [
+export const GroupSessionC2SPayload = z.discriminatedUnion("code", [
   GroupSessionC2SJoinGroupPayload,
   GroupSessionC2SLeaveGroupPayload,
 ]);
--- src/lib/session/s2c-messaging.ts
+++ src/lib/session/s2c-messaging.ts
@@ -3,20 +3,43 @@ import type { GroupSessionC2SCode } from "./c2s-messaging";
 
 export enum GroupSessionS2CCode {
   GroupUpdateStatus = "GroupUpdateStatus",
+  Synchronise = "Synchronise",
 }
 
+type GroupUpdateStatusAction = Extract<
+  "JoinGroup" | "LeaveGroup",
+  keyof typeof GroupSessionC2SCode.enum
+>;
+
+/**
+ * Payload sent in response to a client joining/leaving a group
+ */
 export type GroupSessionS2CGroupUpdateStatusPayload =
   | {
-      status: 0;
-      type: GroupSessionS2CCode.GroupUpdateStatus;
-      action: keyof typeof GroupSessionC2SCode.enum;
-      data: GroupSessionData;
+      status: 0; // group was successfully joined/left
+      code: GroupSessionS2CCode.GroupUpdateStatus;
+      action: GroupUpdateStatusAction;
+      context: {
+        groupName: string;
+        userId: string;
+      };
     }
   | {
-      status: 1;
-      type: GroupSessionS2CCode.GroupUpdateStatus;
-      action: keyof typeof GroupSessionC2SCode.enum;
+      status: 1; // group could not be joined/left
+      code: GroupSessionS2CCode.GroupUpdateStatus;
+      action: GroupUpdateStatusAction;
       error: string;
     };
 
-export type GroupSessionS2CPayload = GroupSessionS2CGroupUpdateStatusPayload;
+/**
+ * Payload sent periodically to ensure the client's data is accurate (in case
+ * some websocket transmissions of {@link GroupSessionS2CGroupUpdateStatusPayload} failed).
+ */
+export type GroupSessionS2CSynchronisePayload = {
+  code: GroupSessionS2CCode.Synchronise;
+  data: GroupSessionData;
+};
+
+export type GroupSessionS2CPayload =
+  | GroupSessionS2CGroupUpdateStatusPayload
+  | GroupSessionS2CSynchronisePayload;
