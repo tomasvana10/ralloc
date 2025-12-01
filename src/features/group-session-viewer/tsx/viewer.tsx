0669260494cd24afd5b46a379031fe66cb225ea4
Tomas Vana <tomasvana@yahoo.com>
2025-12-01 18:49:23 +1100

    better UX + WebSocket status badge for group-session-viewer
    instead of replacing a single spinner with all the viewer content, a mix of skeletons + spinners are now used to minimise layout changes before and after data is obtained from the server

--- src/features/group-session-viewer/tsx/viewer.tsx
+++ src/features/group-session-viewer/tsx/viewer.tsx
@@ -1,9 +1,18 @@
 "use client";
 
-import { BrushCleaning, CircleXIcon, LockIcon, SearchIcon } from "lucide-react";
+import {
+  BrushCleaning,
+  CheckIcon,
+  CircleXIcon,
+  LockIcon,
+  type LucideIcon,
+  SearchIcon,
+  XIcon,
+} from "lucide-react";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { type JSX } from "react";
 import { useRemark } from "react-remark";
+import { ReadyState } from "react-use-websocket-lite";
 import { toast } from "sonner";
 import { ClientAvatar } from "@/components/auth/avatar";
 import {
@@ -24,6 +33,7 @@ import {
 import { Field, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { Skeleton } from "@/components/ui/skeleton";
 import { Spinner } from "@/components/ui/spinner";
 import type { GroupSessionData } from "@/db/group-session";
 import { useIsBelowBreakpoint } from "@/hooks/is-below-breakpoint";
@@ -74,35 +84,52 @@ export function GroupSessionViewer({
     useIsBelowBreakpoint(768),
   ];
   const [groupQuery, setGroupQuery] = React.useState("");
-  const { data, joinGroup, leaveGroup, currentGroup, lock } = useGroupSession({
+  const {
+    data,
+    joinGroup,
+    leaveGroup,
+    currentGroup,
+    freezeThisClient,
+    wsReadyState,
+  } = useGroupSession({
     code,
     thisCompressedUser: userRepresentation.compressedUser,
-    onOpen: () => toast.info("You've been connected to the group session."),
     onClose: (code) => {
       if (code === 1005) return; // likely caused by the user redirecting to a new page, so ignore
-      if (code === GroupSessionS2C.CloseEventCodes.GroupSessionWasDeleted) {
-        toast.error(
-          "This group session was deleted. You'll be redirected in 3 seconds.",
-        );
-        lock();
-        return setTimeout(() => router.push("/"), 3000);
-      }
-      if (code === GroupSessionS2C.CloseEventCodes.RateLimited) {
-        toast.error(
-          "You're sending too many requests. Please try again soon. You'll be redirected in 3 seconds.",
-        );
-        lock();
+
+      if (code > 4000) {
+        // custom close event code
+        let customErrMsgBase = "";
+        switch (code) {
+          case GroupSessionS2C.CloseEventCodes.GroupSessionWasDeleted:
+            customErrMsgBase = "This group session was deleted";
+            break;
+          case GroupSessionS2C.CloseEventCodes.RateLimited:
+            customErrMsgBase =
+              "You're sending too many requests. Please try again soon";
+            break;
+          case GroupSessionS2C.CloseEventCodes.Forbidden:
+            customErrMsgBase =
+              "You're not allowed to perform this action since you aren't the host";
+            break;
+        }
+
+        toast.error(`${customErrMsgBase}. You'll be redirected in 3 seconds.`);
+        freezeThisClient();
         return setTimeout(() => router.push("/"), 3000);
       }
 
-      toast.error(
-        `Your connection was closed (code ${code}). Your client is attempting to reconnect...`,
-      );
+      const defaultErrMsgBase = `Your connection was closed (code ${code})`;
+      if ([1003, 1007, 1008, 1012, 1013, 1014, 1015].includes(code)) {
+        toast.error(`${defaultErrMsgBase}. You'll be redirected in 3 seconds.`);
+        freezeThisClient();
+        return setTimeout(() => router.push("/"), 3000);
+      }
     },
     onError: (msg) => toast.error(msg),
     onReconnectStop: (n) => {
       setTimeout(() => {
-        lock();
+        freezeThisClient();
         toast.warning(
           `Your client has exceeded the maximum amount of reconnection attempts (${n}). You'll be redirected in 3 seconds.`,
         );
@@ -123,17 +150,20 @@ export function GroupSessionViewer({
       .sort((a, b) => groupNameCollator.compare(a.name, b.name));
   }, [data, groupQuery]);
 
-  if (!data) return <Spinner className="size-24 stroke-1 w-full" />;
-
   return (
     <>
-      <GroupSessionInfo
-        data={data}
-        isHost={userRepresentation.userId === data.hostId}
-      />
+      {!data ? (
+        <GroupSessionInfoSkeleton />
+      ) : (
+        <GroupSessionInfo
+          data={data}
+          isHost={userRepresentation.userId === data.hostId}
+          wsReadyState={wsReadyState}
+        />
+      )}
 
       <div className="sm:my-5 my-4">
-        {currentGroup ? (
+        {currentGroup && data ? (
           <div className="grid grid-cols-1 auto-rows-fr">
             <Group
               members={currentGroup.members}
@@ -175,8 +205,10 @@ export function GroupSessionViewer({
         </div>
       </Field>
 
-      {groupCollection.length === 0 ? (
-        <GroupCollectionEmpty />
+      {!data ? (
+        <Spinner className="size-24 stroke-1 w-full" />
+      ) : groupCollection.length === 0 ? (
+        <GroupsEmpty />
       ) : (
         <ScrollArea className="rounded-sm border border-border">
           <div className="max-h-[calc(100vh-19.5rem)] min-h-[75px]">
@@ -215,9 +247,11 @@ export function GroupSessionViewer({
 function GroupSessionInfo({
   data,
   isHost,
+  wsReadyState,
 }: {
   data: GroupSessionData;
   isHost: boolean;
+  wsReadyState: ReadyState;
 }) {
   const repr = UserRepresentation.fromCompressedString(data.compressedHost);
   const [reactMarkdown, setReactMarkdown] = useRemark();
@@ -230,7 +264,7 @@ function GroupSessionInfo({
 
   return (
     <div className="flex flex-col mb-4 gap-2">
-      <div className="flex flex-row items-center sm:gap-4 gap-2">
+      <div className="flex flex-row items-center sm:gap-4 gap-2 min-h-[70px]">
         <ClientAvatar
           image={repr.avatarUrl}
           name={repr.name}
@@ -241,6 +275,7 @@ function GroupSessionInfo({
             <h1 className="text-lg font-semibold leading-none wrap-break-word hyphens-auto">
               {data.name}
             </h1>
+            <WebSocketStatus readyState={wsReadyState} />
             {data.frozen && (
               <Badge variant="outline">
                 <LockIcon />
@@ -289,6 +324,19 @@ function GroupSessionInfo({
   );
 }
 
+function GroupSessionInfoSkeleton() {
+  return (
+    <div className="flex flex-row items-center sm:gap-4 gap-2 min-h-[70px]">
+      <Skeleton className="size-16 rounded-full" />
+      <div className="flex flex-col gap-2">
+        <Skeleton className="h-6 w-[200px]" />
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[150px]" />
+      </div>
+    </div>
+  );
+}
+
 function CurrentGroupEmpty() {
   return (
     <Empty className="border border-dashed p-0! min-h-[90px]">
@@ -302,7 +350,7 @@ function CurrentGroupEmpty() {
   );
 }
 
-function GroupCollectionEmpty() {
+function GroupsEmpty() {
   return (
     <Empty>
       <EmptyHeader>
@@ -313,5 +361,31 @@ function GroupCollectionEmpty() {
         <EmptyDescription>Try a different query.</EmptyDescription>
       </EmptyHeader>
     </Empty>
+  );
+}
+
+const statusData: Record<
+  ReadyState,
+  [
+    string,
+    React.ComponentType,
+    "default" | "outline" | "secondary" | "destructive",
+  ]
+> = {
+  [ReadyState.UNINSTANTIATED]: ["Uninstantiated", XIcon, "outline"],
+  [ReadyState.CONNECTING]: ["Connecting", Spinner, "outline"],
+  [ReadyState.OPEN]: ["Connected", CheckIcon, "outline"],
+  [ReadyState.CLOSING]: ["Closing", Spinner, "outline"],
+  [ReadyState.CLOSED]: ["Closed", XIcon, "destructive"],
+};
+
+function WebSocketStatus({ readyState }: { readyState: ReadyState }) {
+  const [text, Prefix, variant] = statusData[readyState];
+
+  return (
+    <Badge variant={variant}>
+      <Prefix />
+      {text}
+    </Badge>
   );
 }
